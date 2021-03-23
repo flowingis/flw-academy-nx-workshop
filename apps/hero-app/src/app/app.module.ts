1d3e380eb786aa8d0a10bdaa6e3c4045259ed3fa
Luca Del Puppo <luca@delpuppo.net>
2021-03-23 11:14:02 +0100

    add frontend deploy

--- apps/hero-app/src/app/app.module.ts
+++ apps/hero-app/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HeroHeroListModule } from '@flowing/hero/hero-list';
+import { BASE_API_URL } from '@flowing/common/tokens';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, HeroHeroListModule],
-  providers: [],
+  providers: [
+    {
+      provide: BASE_API_URL,
+      useValue: environment.baseApiUrl,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/hero-app/src/environments/environment.prod.ts
+++ apps/hero-app/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
   production: true,
+  baseApiUrl: 'https://t9fcz0ya4h.execute-api.eu-central-1.amazonaws.com/prod',
 };
